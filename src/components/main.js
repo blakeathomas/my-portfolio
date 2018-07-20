36d227b410d22b3fe6a2c8bf3826069ec143c9cd
Blake Thomas <blakethomas0101@gmail.com>
2018-07-20 09:35:10 -0400

    resolved switch/router error

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import landingpage from "./landingpage";
 import aboutme from "./aboutme";
 import contact from "./contact";
@@ -7,13 +7,15 @@ import projects from "./projects";
 import resume from "./resume";
 
 const Main = () => (
-  <Switch>
-    <Route exact path="/" component={landingpage} />
-    <Route path="/aboutme" component={aboutme} />
-    <Route path="/contact" component={contact} />
-    <Route path="/projects" component={projects} />
-    <Route path="/resume" component={resume} />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={landingpage} />
+      <Route path="/aboutme" component={aboutme} />
+      <Route path="/contact" component={contact} />
+      <Route path="/projects" component={projects} />
+      <Route path="/resume" component={resume} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Main;
